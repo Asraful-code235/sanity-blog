1ed7a60e6731bde17956df64a6f6eb7ec7b3ed61
Asraful-code235 <asrafulislamcodes@gmail.com>
2024-06-30 19:35:29 +0600

    initial commit

---
+++ app/(website)/about/about.js
@@ -0,0 +1,57 @@
+import Container from "@/components/container";
+import { urlForImage } from "@/lib/sanity/image";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function About({ authors, settings }) {
+  return (
+    <Container>
+      <h1 className="text-brand-primary mb-3 mt-2 text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
+        About
+      </h1>
+      <div className="text-center">
+        <p className="text-lg">Get to know me.</p>
+      </div>
+
+      <div className=" flex flex-wrap items-center justify-center md:gap-16">
+        {authors.slice(0, 3).map(author => {
+          const imageProps = urlForImage(author?.image) || null;
+          return (
+            <div
+              key={author._id}
+              className="relative aspect-square overflow-hidden rounded-md bg-slate-50 odd:translate-y-10 odd:md:translate-y-16">
+              <div className="h-28 w-28">
+                {imageProps && (
+                  <Image
+                    src={imageProps?.src}
+                    alt={author?.name || " "}
+                    fill
+                    sizes="(max-width: 320px) 100vw, 320px"
+                    className="h-full w-full object-cover"
+                  />
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+
+      <div className="prose mx-auto  mt-14 pt-14 text-center dark:prose-invert">
+        <p>
+          We provide real-time connectivity to enable software
+          providers and financial institutions to build integrated
+          products for their small business customers.
+        </p>
+        <p>
+          Our API infrastructure is leveraged by clients ranging from
+          lenders to corporate card providers and business forecasting
+          tools, with use cases including automatic reconciliation,
+          business dashboarding, and loan decisioning.
+        </p>
+        <p>
+          <Link href="/contact">Get in touch</Link>
+        </p>
+      </div>
+    </Container>
+  );
+}
---
+++ components/sidebar.js
@@ -0,0 +1,76 @@
+import SearchInput from "@/components/ui/search";
+import { urlForImage } from "@/lib/sanity/image";
+import Image from "next/image";
+import Link from "next/link";
+import Label from "@/components/ui/label";
+import DateTime from "@/components/ui/time";
+
+export default function Sidebar(props) {
+  return (
+    <div className="mt-5 font-sans">
+      <Searchbar />
+
+      {props.related && (
+        <RelatedPosts
+          related={props.related}
+          pathPrefix={props.pathPrefix}
+        />
+      )}
+    </div>
+  );
+}
+
+function Searchbar() {
+  return (
+    <div>
+      <h3 className="text-2xl font-bold dark:text-white">
+        Search Posts
+      </h3>
+      <form action="/search" method="GET" className="mt-4">
+        <SearchInput placeholder="Search" />
+      </form>
+    </div>
+  );
+}
+
+function RelatedPosts({ related, pathPrefix }) {
+  return (
+    <div className="mt-10">
+      <h3 className="text-2xl font-bold dark:text-white">Related</h3>
+      <div className="mt-6 grid gap-6">
+        {related.slice(0, 3).map((item, index) => {
+          const imageProps = item?.image
+            ? urlForImage(item?.image)
+            : null;
+          return (
+            <Link
+              key={index}
+              href={`/post/${pathPrefix ? `${pathPrefix}/` : ""}${
+                item.slug.current
+              }`}>
+              <div className="flex gap-5">
+                <div className="relative h-20 w-24 shrink-0 overflow-hidden rounded-md">
+                  <Image
+                    src={imageProps.src}
+                    alt={item.title || "Thumbnail"}
+                    fill
+                    sizes="100vw"
+                    className="object-cover"
+                  />
+                </div>
+                <div>
+                  <h3 className="font-medium dark:text-white">
+                    {item.title}
+                  </h3>
+                  <p className="mt-2 text-sm text-gray-500">
+                    <DateTime date={item.date} />
+                  </p>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
